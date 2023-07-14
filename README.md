a61083f51f193ff2adb51ba044db8eab5432167a
olakrhoff <ola.kr@hotmail.no>
2023-07-14 22:47:23 +0200

    Added example file, and check of opening file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # RFC-Data-Format-Printer
+
+The example input file describes the Internet Header Format from rfc791
--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
+#include <fstream>
 
-int main()
+using namespace std;
+
+int main(int argc, char **argv)
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    if (argc != 2)
+    {
+        cout << "Not correct amount of arguments. Expected 1, but got " << argc - 1 << endl;
+        cout << "Usage: ./<program_name> <file_path>" << endl;
+        cout << "Example: ./main example/path/to/file.txt" << endl;
+        return EXIT_FAILURE;
+    }
+    
+    const char* file_path = argv[1];
+    cout << file_path << endl;
+    // Open the file
+    ifstream file(file_path);
+    if (!file.is_open()) {
+        cout << "Failed to open the file." << endl;
+        return 1;
+    }
+    
+    // Read and process the file contents
+    string line;
+    while (getline(file, line)) {
+        // Process each line as needed
+        cout << line << endl;
+    }
+    
+    // Close the file
+    file.close();
+    
+    
+    return EXIT_SUCCESS;
 }
